dbcfe6d6e4d8ae8f243d25465c57c098a521ff3c
chikin030611 <chikin030611@gmail.com>
2024-07-29 17:19:38 +0800

    Add audio playing

--- src/ColorMC.Gui/UI/Flyouts/Live2DFlyout.cs
+++ src/ColorMC.Gui/UI/Flyouts/Live2DFlyout.cs
@@ -1,11 +1,37 @@
 ﻿using System;
+using System.IO;
+using System.Media;
 using System.Collections.Generic;
 using ColorMC.Gui.UI.Controls.Main;
+using Silk.NET.SDL;
 
 namespace ColorMC.Gui.UI.Flyouts;
 
 public class Live2DFlyout
-{
+{   
+    private void PlayIntroAudio()
+    {
+        try
+        {
+            // Get the base directory of the application
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Construct the relative path to the audio file
+            string audioFilePath = Path.Combine(baseDirectory, "Resource", "Audio", "intro.wav");
+
+            using (SoundPlayer player = new SoundPlayer(audioFilePath))
+            {
+                player.Load();
+                player.Play();
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions (e.g., file not found, format issues)
+            Console.WriteLine($"Error playing audio: {ex.Message}");
+        }
+    }
+
     public Live2DFlyout(Live2dRender live2d)
     {
         _ = new FlyoutsControl(
@@ -36,6 +62,10 @@ public class Live2DFlyout
                     _ = new FlyoutsControl([.. list1], live2d);
                 }
             }),
+            ("Speak intro", true, ()=>
+            {
+                PlayIntroAudio();
+            })
         ], live2d);
     }
 }
