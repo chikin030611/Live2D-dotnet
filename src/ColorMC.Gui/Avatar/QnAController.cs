6476a648bc1891ec9a82f798bb13265a847a56f3
chikin030611 <chikin030611@gmail.com>
2024-07-31 18:33:44 +0800

    Add QnAController

---
+++ src/ColorMC.Gui/Avatar/QnAController.cs
@@ -0,0 +1,28 @@
+﻿using ColorMC.Gui.AudioPlayer;
+using ColorMC.Gui.UI.Controls.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorMC.Gui.Avatar;
+
+public class QnAController
+{
+    Live2dRender live2d;
+
+    public QnAController(Live2dRender live2d)
+    {
+        this.live2d = live2d;
+    }
+
+    public void StartLive2dSpeaking(int qnum)
+    {
+        QnAMapper qnAMapper = new QnAMapper();
+        string path = qnAMapper.GetAudioFilePath(qnum);
+
+        AudioPlayer.AudioPlayer.PlayAudio(path);
+        live2d.StartSpeaking(path);
+    }
+}
--- src/ColorMC.Gui/UI/Flyouts/Live2DFlyout.cs
+++ src/ColorMC.Gui/UI/Flyouts/Live2DFlyout.cs
@@ -6,24 +6,16 @@ using ColorMC.Gui.UI.Controls.Main;
 using Silk.NET.SDL;
 using ColorMC.Gui.AudioPlayer;
 using Live2DCSharpSDK.App;
+using ColorMC.Gui.Avatar;
 
 namespace ColorMC.Gui.UI.Flyouts;
 
 public class Live2DFlyout
 {
-    private void PlayAudio(Live2dRender live2d, int qnum)
-    {
-        // Parameter: location of audio file
-        // Find file path of audio according question number
-        QnAMapper qnAMapper = new QnAMapper();
-        string path = qnAMapper.GetAudioFilePath(qnum);
-
-        AudioPlayer.AudioPlayer.PlayAudio(path);
-        live2d.StartSpeaking(path);
-    }
-
     public Live2DFlyout(Live2dRender live2d)
     {
+        QnAController controller = new QnAController(live2d);
+
         var flyoutItems = new List<(string, bool, Action)>
         {
             (App.Lang("Live2dControl.Flyouts.Text1"), true, () =>
@@ -52,7 +44,7 @@ public class Live2DFlyout
                     _ = new FlyoutsControl([.. list1], live2d);
                 }
             }),
-            ("Speak intro", true, () => PlayAudio(live2d, 0))
+            ("Speak intro", true, (Action)(() => controller.StartLive2dSpeaking(0)))
         };
 
         // Add questions dynamically
@@ -61,7 +53,7 @@ public class Live2DFlyout
         for (int i = 1; i <= numberOfQuestions; i++)
         {
             int questionNumber = i; // Capture the loop variable
-            flyoutItems.Add(($"Question {questionNumber}", true, () => PlayAudio(live2d, questionNumber)));
+            flyoutItems.Add(($"Question {questionNumber}", true, (Action)(() => controller.StartLive2dSpeaking(questionNumber))));
         }
 
         _ = new FlyoutsControl([.. flyoutItems], live2d);
