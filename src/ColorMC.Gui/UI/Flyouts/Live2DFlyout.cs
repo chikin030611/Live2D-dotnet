af821808b916f84c928ae7f4ad2f2792db9441fc
chikin030611 <chikin030611@gmail.com>
2024-07-31 15:27:22 +0800

    Add model speaking animation

--- src/ColorMC.Gui/UI/Flyouts/Live2DFlyout.cs
+++ src/ColorMC.Gui/UI/Flyouts/Live2DFlyout.cs
@@ -5,18 +5,21 @@ using System.Collections.Generic;
 using ColorMC.Gui.UI.Controls.Main;
 using Silk.NET.SDL;
 using ColorMC.Gui.AudioPlayer;
+using Live2DCSharpSDK.App;
 
 namespace ColorMC.Gui.UI.Flyouts;
 
 public class Live2DFlyout
 {
-    private void PlayAudio(int qnum)
+    private void PlayAudio(Live2dRender live2d, int qnum)
     {
         // Parameter: location of audio file
         // Find file path of audio according question number
         QnAMapper qnAMapper = new QnAMapper();
         string path = qnAMapper.GetAudioFilePath(qnum);
-        AudioPlayer.AudioPlayer.PlayAudio(path);
+
+        // TODO: find model.lappwavfilehandler
+        live2d.StartSpeaking(path);
     }
 
     public Live2DFlyout(Live2dRender live2d)
@@ -49,7 +52,7 @@ public class Live2DFlyout
                     _ = new FlyoutsControl([.. list1], live2d);
                 }
             }),
-            ("Speak intro", true, () => PlayAudio(0))
+            ("Speak intro", true, () => PlayAudio(live2d, 0))
         };
 
         // Add questions dynamically
@@ -58,7 +61,7 @@ public class Live2DFlyout
         for (int i = 1; i <= numberOfQuestions; i++)
         {
             int questionNumber = i; // Capture the loop variable
-            flyoutItems.Add(($"Question {questionNumber}", true, () => PlayAudio(questionNumber)));
+            flyoutItems.Add(($"Question {questionNumber}", true, () => PlayAudio(live2d, questionNumber)));
         }
 
         _ = new FlyoutsControl([.. flyoutItems], live2d);
