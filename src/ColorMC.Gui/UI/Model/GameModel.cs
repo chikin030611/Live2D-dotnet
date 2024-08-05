b2fa743970741e69aabd61592698b0ee2966cf6e
chikin030611 <chikin030611@gmail.com>
2024-08-05 09:47:34 +0800

    Remove models

--- src/ColorMC.Gui/UI/Model/GameModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using ColorMC.Core.Objs;
-
-namespace ColorMC.Gui.UI.Model;
-
-public abstract partial class GameModel(BaseModel model, GameSettingObj obj) : TopModel(model)
-{
-    public GameSettingObj Obj => obj;
-}
