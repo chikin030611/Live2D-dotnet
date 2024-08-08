20d06a61f5811e714dd2e7751afb787e409c48c3
chikin030611 <chikin030611@gmail.com>
2024-08-08 12:27:17 +0800

    Add stop audio

--- src/Live2DDotNet/Avatar/AudioPlayer.cs
+++ src/Live2DDotNet/Avatar/AudioPlayer.cs
@@ -4,19 +4,30 @@ namespace Live2DDotNet.Avatar;
 
 public static class AudioPlayer
 {
+    static readonly SoundPlayer player = new();
+
     public static void PlayAudio(string path)
     {
         try
         {
-            using (SoundPlayer player = new SoundPlayer(path))
-            {
-                player.Load();
-                player.Play();
-            }
+            player.SoundLocation = path;
+            player.Load();
+            player.Play();
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error playing audio: {ex.Message}");
+        }
+    }
+
+    public static void StopAudio() {
+        try
+        {
+            player.Stop();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error stopping audio: {ex.Message}");
         }
     }
 }
